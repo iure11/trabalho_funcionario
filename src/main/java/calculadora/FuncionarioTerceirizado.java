045711df4ab24e6f611732fdb96432c96f1b1c68
iure11 <iurematheus11@gmail.com>
2023-04-13 18:45:32 -0300

    Alterando classe FuncionarioTerceirizado

--- src/main/java/calculadora/FuncionarioTerceirizado.java
+++ src/main/java/calculadora/FuncionarioTerceirizado.java
@@ -1,5 +1,32 @@
 package calculadora;
 
-public class FuncionarioTerceirizado {
+public class FuncionarioTerceirizado extends Funcionario {
+    private double despesasAdicionais;
 
+    public FuncionarioTerceirizado(String nome, int horasTrabalhadas, double valorHora, double despesasAdicionais) {
+        super(nome, horasTrabalhadas, valorHora);
+        this.despesasAdicionais = despesasAdicionais;
+    }
+
+    @Override
+    public double calcularPagamento() {
+        double pagamento = super.calcularPagamento() + (despesasAdicionais * 1.10);
+        if (pagamento < 1320.00) {
+            pagamento = 1320.00;
+        }
+        return pagamento;
+    }
+
+    // Getters e Setters
+    public double getDespesasAdicionais() {
+        return despesasAdicionais;
+    }
+
+    public void setDespesasAdicionais(double despesasAdicionais) {
+        if (despesasAdicionais > 1000.00) {
+            this.despesasAdicionais = 1000.00;
+        } else {
+            this.despesasAdicionais = despesasAdicionais;
+        }
+    }
 }
